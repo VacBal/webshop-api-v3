a467b3b57e03785d11bc5bd12286bea35d3beff5
VacBal <vaczi.balazs.zsolt@gmail.com>
2024-11-29 10:19:35 +0100

    elso commit

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  
+  
+
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Üdvözlünk a Webshopban!</h1>
+      <p>
+        Szia, Örülünk, hogy itt vagy.
+      </p>
+    </div>
+  );
+};
+
+export default HomePage;
